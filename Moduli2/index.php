23792dd619feccd8493542a509c96a15947c52b6
Jon Z <jonzuka31@gmail.com>
2026-01-17 14:46:06 +0100

    1

---
+++ Moduli2/index.php
@@ -0,0 +1,19 @@
+<?php
+    $school_Name = "Digital School";
+
+    echo("I go to $school_Name")."<hr>";
+
+    $numri = 2;
+
+    echo("i am in level $numri")."<hr>";
+
+    $the_String = "ju jeni klasa ma e mira"; 
+
+    echo strlen($the_String)."<hr>";
+
+    echo str_word_count($the_String)."<hr>";
+
+    echo str_replace("mira","zhurmshmja", $the_String)."<hr>";
+
+    echo strrev($the_String)."<hr>";
+?>
